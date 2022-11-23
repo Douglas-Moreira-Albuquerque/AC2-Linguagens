9a1ba4e086856287210f993c02c207cca74aab47
Douglas-Moreira-Albuquerque <douglas_dealbuquerque@hotmail.com>
2022-11-22 21:03:49 -0300

    AC2 de Linguagens de Programacao

---
+++ src/trabalhorobson/Clientes.java
@@ -0,0 +1,65 @@
+package trabalhorobson;
+
+public class Clientes {
+    
+    private String NOME;
+    private String CPF;
+    private String TELEFONE;
+    private String CIDADE;
+    private String UF;
+    private String EMAIL;
+    
+    public Clientes() {
+        
+    } 
+
+    public String getNOME() {
+        return NOME;
+    }
+
+    public void setNOME(String NOME) {
+        this.NOME = NOME;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTELEFONE() {
+        return TELEFONE;
+    }
+
+    public void setTELEFONE(String TELEFONE) {
+        this.TELEFONE = TELEFONE;
+    }
+
+    public String getCIDADE() {
+        return CIDADE;
+    }
+
+    public void setCIDADE(String CIDADE) {
+        this.CIDADE = CIDADE;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
+
+    public String getEMAIL() {
+        return EMAIL;
+    }
+
+    public void setEMAIL(String EMAIL) {
+        this.EMAIL = EMAIL;
+    }
+    
+    
+}
